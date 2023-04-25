0bc03fc00377135f0d25bcb5f31d68e4748a7354
Maximiliano Pérez <maximiliano.leonel.perez1999@gmail.com>
2023-04-25 10:10:44 -0300

    Adding some changes

--- index.js
+++ index.js
@@ -1,42 +1,41 @@
-require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const app = express();
 const cors = require("cors");
+require("dotenv").config();
+const Person = require("./models/persons");
 app.use(cors());
 app.use(express.json());
 app.use(express.static("dist"));
 
-const Person = require("./models/persons");
-
-let persons = [
-  {
-    name: "Arto Hellas",
-    number: "040-123456",
-    id: 1,
-  },
-  {
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-    id: 2,
-  },
-  {
-    name: "Dan Abramov",
-    number: "12-43-234345",
-    id: 3,
-  },
-  {
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-    id: 4,
-  },
-];
+// let persons = [
+//   {
+//     name: "Arto Hellas",
+//     number: "040-123456",
+//     id: 1,
+//   },
+//   {
+//     name: "Ada Lovelace",
+//     number: "39-44-5323523",
+//     id: 2,
+//   },
+//   {
+//     name: "Dan Abramov",
+//     number: "12-43-234345",
+//     id: 3,
+//   },
+//   {
+//     name: "Mary Poppendieck",
+//     number: "39-23-6423122",
+//     id: 4,
+//   },
+// ];
 
 app.use(morgan("tiny"));
-// Persons
 
+// Persons
 app.get("/", (req, res) => {
-  response.send("<h1>Phonebook</h1>");
+  response.send("<h1>Phonebook Master</h1>");
 });
 app.get("/api/persons", (request, response) => {
   Person.find({}).then((persons) => {
@@ -47,7 +46,7 @@ app.get("/api/persons", (request, response) => {
 // Info
 app.get("/info", (request, response) => {
   response.send(`<div>
-    <p>Phonebook has info for ${persons.length} people</p>
+    <p>Phonebook has info for x people</p>
     <p>${new Date()}</p>
     </div>`);
 });
@@ -55,7 +54,7 @@ app.get("/info", (request, response) => {
 
 // Create a person
 app.post("/api/persons", (request, response) => {
-  let per = request.body;
+  let body = request.body;
 
   // if (per.name === "" || per.number === "") {
   //   response.status(409).json({ error: "Name or number is missing" });
@@ -111,10 +110,7 @@ app.delete("/api/persons/:id", (request, response) => {
 });
 //////////////
 
-const getRandomNumber = (max) => {
-  return Math.floor(Math.random() * max);
-};
-
 const PORT = process.env.PORT;
-app.listen(PORT);
-console.log(`Server running on port ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
